518ee5fcbb4d6a80bc7fb8ab5338cef1d39a0923
sunghun <leedool3003@gmail.com>
2025-03-09 14:47:15 +0900

    :sparkles: [feat] set home view

--- Projects/Feature/Scene/Home/Sources/HomeCore.swift
+++ Projects/Feature/Scene/Home/Sources/HomeCore.swift
@@ -6,7 +6,12 @@
 //  Copyright © 2025 com.azhy.julook. All rights reserved.
 //
 
+import Foundation
+
+import Core
+
 import ComposableArchitecture
+import Supabase
 
 @Reducer
 public struct HomeCore {
@@ -15,18 +20,143 @@ public struct HomeCore {
   @ObservableState
   public struct State: Equatable {
     public init() { }
+    
+    var isLoadingNewReleases: Bool = false
+    var newReleases: [Makgeolli] = []
+    var newReleasesImages: [UUID: URL] = [:]
+    
+    var isLoadingAwards: Bool = false
+    var awards: [Award] = []
   }
   
   public enum Action {
     case onAppear
+    
+    case fetchNewReleases
+    case newReleasesResponse(TaskResult<[Makgeolli]>)
+    
+    case fetchNewReleasesImage(Makgeolli)
+    case newReleasesImageResponse(id: UUID, TaskResult<URL>)
+    
+    case fetchAwards
+    case awardsResponse(TaskResult<[Award]>)
+    
+    case logError(HomeCoreError)
   }
+  
+  private enum Constants {
+    static let storageBucket = "makgeolli_image"
+  }
+  
+  @Dependency(\.supabaseClient) var supabaseClient
   
   public var body: some Reducer<State, Action> {
     Reduce { state, action in
       switch action {
       case .onAppear:
+        return .merge(
+          .send(.fetchNewReleases),
+          .send(.fetchAwards)
+        )
+        
+      case .fetchNewReleases:
+        state.isLoadingNewReleases = true
+        let supabaseClient = self.supabaseClient
+        return .run { send in
+          do {
+            let makgeollis = try await supabaseClient.fetchNewReleases()
+            await send(.newReleasesResponse(.success(makgeollis)))
+          } catch {
+            await send(.newReleasesResponse(.failure(error)))
+          }
+        }
+        
+      case let .newReleasesResponse(.success(makgeollis)):
+        state.isLoadingNewReleases = false
+        state.newReleases = makgeollis
+        return .merge(
+          makgeollis.compactMap { makgeolli in
+            return .send(.fetchNewReleasesImage(makgeolli))
+          }
+        )
+        
+      case let .newReleasesResponse(.failure(error)):
+        state.isLoadingNewReleases = false
+        return .send(.logError(HomeCoreError(
+          code: .failToFetchNewReleases,
+          underlying: error
+        )))
+        
+      case let .fetchNewReleasesImage(makgeolli):
+        guard let imageName = makgeolli.imageName else {
+          return .none
+        }
+        
+        let supabaseClient = self.supabaseClient
+        return .run { send in
+          do {
+            let fileName = imageName.hasSuffix(".png") ? imageName : "\(imageName).png"
+            let publicURL = try await supabaseClient.getPublicURL(Constants.storageBucket, fileName)
+            await send(.newReleasesImageResponse(id: makgeolli.id, .success(publicURL)))
+          } catch {
+            await send(.newReleasesImageResponse(id: makgeolli.id, .failure(error)))
+          }
+        }
+        
+      case let .newReleasesImageResponse(id, .success(url)):
+        state.newReleasesImages[id] = url
         return .none
+        
+      case let .newReleasesImageResponse(_, .failure(error)):
+        return .send(.logError(HomeCoreError(
+          code: .failToFetchImage,
+          underlying: error
+        )))
+        
+      case .fetchAwards:
+        state.isLoadingAwards = true
+        let supabaseClient = self.supabaseClient
+        
+        return .run { send in
+          do {
+            let awards = try await supabaseClient.fetchAwards()
+            await send(.awardsResponse(.success(awards)))
+          } catch {
+            await send(.awardsResponse(.failure(error)))
+          }
+        }
+        
+      case let .awardsResponse(.success(awards)):
+        state.isLoadingAwards = false
+        state.awards = awards
+        return .none
+        
+      case let .awardsResponse(.failure(error)):
+        state.isLoadingAwards = false
+        return .send(.logError(HomeCoreError(
+          code: .failToFetchAwards,
+          underlying: error
+        )))
+        
+      case let .logError(error):
+        return .run { _ in
+          Log.error(error)
+        }
       }
     }
+  }
+}
+
+public struct HomeCoreError: JulookError, @unchecked Sendable {
+  public var userInfo: [String: Any] = [:]
+  public var code: Code
+  public var underlying: Error?
+  
+  public enum Code: Int, Sendable {
+    case failToSupabaseClientInitialized
+    case failToFetchNewReleases
+    case failToGetImageUrl
+    case failToFetchImage
+    case failToFetchAwards
   }
 }
--- Projects/Feature/Scene/Home/Sources/HomeView.swift
+++ Projects/Feature/Scene/Home/Sources/HomeView.swift
@@ -7,19 +7,387 @@
 
 import SwiftUI
 
+import DesignSystem
+import Core
+
 import ComposableArchitecture
 
-public struct ContentView: View {
-  @Bindable var store: StoreOf<HomeCore>
+public struct HomeView: View {
+  let store: StoreOf<HomeCore>
   
   public init(store: StoreOf<HomeCore>) {
     self.store = store
   }
   
   public var body: some View {
-    VStack {
-      Text("테스트")
+    ZStack {
+      DesignSystemAsset.Colors.darkbase.swiftUIColor
+        .ignoresSafeArea()
+      
+      ScrollView(showsIndicators: false) {
+        VStack(spacing: 20) {
+          HeaderView()
+          
+          MakgeolliFilterView()
+          
+          NewReleasesView(store: store)
+          
+          MakgeolliTopicView(store: store)
+        }
+      }
+      .padding(.vertical, 20)
     }
-    .background(.yellow)
+    .onAppear {
+      // TODO: 스플래시 뷰 추가하고, supabase init 하고 홈으로 넘어가는 로직 추가하면 sleep 삭제
+      Task {
+        try await Task.sleep(for: .seconds(1))
+        store.send(.onAppear)
+      }
+    }
+  }
+}
+
+// MARK: - HeaderView
+
+private struct HeaderView: View {
+  private let randomProfileIndex: Int
+  
+  fileprivate init() {
+    self.randomProfileIndex = Int.random(in: 1...8)
+  }
+  
+  fileprivate var body: some View {
+    HStack {
+      Text("모아보기")
+        .foregroundColor(.w)
+        .font(.SFTitle)
+      
+      Spacer()
+      
+      randomProfileImage
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 30, height: 30)
+    }
+    .padding(.horizontal, 16)
+  }
+  
+  private var randomProfileImage: Image {
+    switch randomProfileIndex {
+    case 1: return DesignSystemAsset.Images.p1.swiftUIImage
+    case 2: return DesignSystemAsset.Images.p2.swiftUIImage
+    case 3: return DesignSystemAsset.Images.p3.swiftUIImage
+    case 4: return DesignSystemAsset.Images.p4.swiftUIImage
+    case 5: return DesignSystemAsset.Images.p5.swiftUIImage
+    case 6: return DesignSystemAsset.Images.p6.swiftUIImage
+    case 7: return DesignSystemAsset.Images.p7.swiftUIImage
+    case 8: return DesignSystemAsset.Images.p8.swiftUIImage
+    default: return DesignSystemAsset.Images.p1.swiftUIImage
+    }
+  }
+}
+
+private struct MakgeolliFilterView: View {
+  fileprivate init() {
+  }
+  
+  fileprivate var body: some View {
+    VStack(spacing: 20) {
+      HStack(alignment: .center, spacing: 8) {
+        Text("특징으로 찾기")
+          .foregroundColor(.w)
+          .font(.SF20B)
+        
+        DesignSystemAsset.Images.arrowRight.swiftUIImage
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(height: 20)
+          .foregroundColor(.w)
+        
+        Spacer()
+      }
+      .padding(.horizontal, 16)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 16) {
+          ForEach(FilterType.allCases) { filterType in
+            VStack(spacing: 8) {
+              filterType.image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 70, height: 70)
+                .background(Circle().fill(Color.darkgray))
+                .clipShape(Circle())
+              
+              Text(filterType.description)
+                .foregroundColor(.w85)
+                .font(.SF12B)
+            }
+          }
+        }
+        .padding(.horizontal, 16)
+      }
+    }
+    .padding(.bottom, 20)
+  }
+  
+  private enum FilterType: String, CaseIterable, Identifiable {
+    case thick
+    case sweet
+    case sour
+    case carbonation
+    case aspartame
+    
+    var id: String { self.rawValue }
+    
+    var image: Image {
+      switch self {
+      case .thick: return DesignSystemAsset.Images.thick.swiftUIImage
+      case .sweet: return DesignSystemAsset.Images.sweet.swiftUIImage
+      case .sour: return DesignSystemAsset.Images.sour.swiftUIImage
+      case .carbonation: return DesignSystemAsset.Images.carbonation.swiftUIImage
+      case .aspartame: return DesignSystemAsset.Images.aspartame.swiftUIImage
+      }
+    }
+    
+    var description: String {
+      switch self {
+      case .thick: return "걸쭉한"
+      case .sweet: return "달달한"
+      case .sour: return "시큼한"
+      case .carbonation: return "탄산감 많은"
+      case .aspartame: return "아스파탐 없는"
+      }
+    }
+  }
+}
+
+// MARK: - NewReleasesView
+
+private struct NewReleasesView: View {
+  let store: StoreOf<HomeCore>
+  
+  fileprivate init(store: StoreOf<HomeCore>) {
+    self.store = store
+  }
+  
+  fileprivate var body: some View {
+    VStack(spacing: 20) {
+      HStack(alignment: .center, spacing: 8) {
+        Text("새로 나왔어요")
+          .foregroundColor(.w)
+          .font(.SF20B)
+        Spacer()
+      }
+      .padding(.horizontal, 16)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        LazyHStack(spacing: 16) {
+          if store.isLoadingNewReleases {
+            ForEach(0..<3, id: \.self) { idx in
+              RoundedRectangle(cornerRadius: 16)
+                .fill(Color.darkgray)
+                .frame(width: 104, height: 240)
+                .overlay {
+                  ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .w))
+                }
+                .padding(.leading, idx == 0 ? 16 : 0)
+            }
+          } else {
+            ForEach(Array(store.newReleases.enumerated()), id: \.element.id) { idx, makgeolli in
+              RoundedRectangle(cornerRadius: 16)
+                .fill(Color.darkgray)
+                .frame(width: 104, height: 240)
+                .overlay {
+                  VStack(spacing: 12) {
+                    if let imageUrl = store.newReleasesImages[makgeolli.id] {
+                      AsyncImage(url: imageUrl) { phase in
+                        makeImageView(for: phase)
+                      }
+                    } else {
+                      ProgressView()
+                        .frame(width: 50, height: 114)
+                    }
+                    
+                    Text(makgeolli.name)
+                      .foregroundColor(.w)
+                      .font(.SF12R)
+                      .lineLimit(1)
+                    
+                    VStack(spacing: 0) {
+                      Spacer()
+                      HStack(alignment: .bottom, spacing: 4) {
+                        VStack(spacing: 4) {
+                          getChartImage(for: makgeolli.sweetness)
+                          Text("단")
+                            .foregroundColor(.w50)
+                            .font(.SF10B)
+                        }
+                        
+                        VStack(spacing: 4) {
+                          getChartImage(for: makgeolli.sourness)
+                          Text("신")
+                            .foregroundColor(.w50)
+                            .font(.SF10B)
+                        }
+                        
+                        VStack(spacing: 4) {
+                          getChartImage(for: makgeolli.thickness)
+                          Text("걸")
+                            .foregroundColor(.w50)
+                            .font(.SF10B)
+                        }
+                        
+                        VStack(spacing: 4) {
+                          getChartImage(for: makgeolli.carbonation)
+                          Text("탄")
+                            .foregroundColor(.w50)
+                            .font(.SF10B)
+                        }
+                      }
+                    }
+                  }
+                  .padding(20)
+                }
+                .padding(.leading, idx == 0 ? 16 : 0)
+            }
+          }
+        }
+        .padding(.trailing, 16)
+      }
+    }
+    .padding(.bottom, 20)
+  }
+}
+
+private extension NewReleasesView {
+  func makeImageView(for phase: AsyncImagePhase) -> some View {
+    switch phase {
+    case .empty:
+      return AnyView(
+        ProgressView()
+          .frame(width: 50, height: 114)
+      )
+    case .success(let image):
+      return AnyView(
+        image
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 50, height: 114)
+      )
+    case .failure:
+      return AnyView(defaultMakgeolliImage())
+      
+    @unknown default:
+      return AnyView(defaultMakgeolliImage())
+    }
+  }
+  
+  func defaultMakgeolliImage() -> some View {
+    DesignSystemAsset.Images.defaultMakgeolli.swiftUIImage
+      .resizable()
+      .aspectRatio(contentMode: .fit)
+      .frame(width: 50, height: 114)
+  }
+}
+
+private extension NewReleasesView {
+  func getChartImage(for value: Int?) -> Image {
+    guard let value = value else {
+      return DesignSystemAsset.Images.nillChart.swiftUIImage
+    }
+    
+    switch value {
+    case 0:
+      return DesignSystemAsset.Images._0Chart.swiftUIImage
+    case 1:
+      return DesignSystemAsset.Images._1Chart.swiftUIImage
+    case 2:
+      return DesignSystemAsset.Images._2Chart.swiftUIImage
+    case 3:
+      return DesignSystemAsset.Images._3Chart.swiftUIImage
+    case 4:
+      return DesignSystemAsset.Images._4Chart.swiftUIImage
+    case 5:
+      return DesignSystemAsset.Images._5Chart.swiftUIImage
+    default:
+      return DesignSystemAsset.Images.nillChart.swiftUIImage
+    }
+  }
+}
+
+// MARK: - MakgeolliTopicView
+
+private struct MakgeolliTopicView: View {
+  let store: StoreOf<HomeCore>
+  
+  fileprivate init(store: StoreOf<HomeCore>) {
+    self.store = store
+  }
+  
+  fileprivate var body: some View {
+    VStack(spacing: 20) {
+      HStack(alignment: .center, spacing: 8) {
+        Text("주제로 찾기")
+          .foregroundColor(.w)
+          .font(.SF20B)
+        Spacer()
+      }
+      .padding(.horizontal, 16)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        LazyHStack(spacing: 16) {
+          if store.isLoadingAwards {
+            ForEach(0..<3, id: \.self) { idx in
+              RoundedRectangle(cornerRadius: 16)
+                .fill(Color.darkgray)
+                .frame(width: 160, height: 100)
+                .overlay {
+                  ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .w))
+                }
+                .padding(.leading, idx == 0 ? 16 : 0)
+            }
+          } else {
+            ForEach(Array(store.awards.enumerated()), id: \.element.id) { idx, award in
+              if award.type == "korea_award" {
+                RoundedRectangle(cornerRadius: 16)
+                  .fill(LinearGradient.warmNeutral)
+                  .frame(width: 160, height: 100)
+                  .overlay {
+                    HStack {
+                      VStack {
+                        Spacer()
+                        let components = award.name.components(separatedBy: " ")
+                        VStack(alignment: .leading, spacing: 0) {
+                          ForEach(components, id: \.self) { component in
+                            Text(component)
+                              .foregroundColor(.w)
+                              .font(.SF12B)
+                          }
+                        }
+                        .padding(.bottom, 16)
+                      }
+                      Spacer()
+                      VStack {
+                        DesignSystemAsset.Images.koreaAwardsLogo.swiftUIImage
+                          .resizable()
+                          .aspectRatio(contentMode: .fit)
+                          .frame(height: 48)
+                          .padding(.top, 16)
+                        Spacer()
+                      }
+                    }
+                    .padding(.horizontal, 16)
+                  }
+                  .padding(.leading, idx == 0 ? 16 : 0)
+              }
+            }
+          }
+        }
+      }
+    }
+    .padding(.bottom, 20)
   }
 }
