5067c2d2fd7e022fb87277521e254e61619b5db5
sunghun <leedool3003@gmail.com>
2025-03-09 19:08:09 +0900

    :lipstick: [style] 17B 폰트 추가

--- Projects/DesignSystem/Sources/Fonts/Font+extensions.swift
+++ Projects/DesignSystem/Sources/Fonts/Font+extensions.swift
@@ -17,8 +17,14 @@ extension Font {
       return .system(size: 24, weight: .bold)
     case .SF20B:
       return .system(size: 20, weight: .bold)
+    case .SF17B:
+      return .system(size: 17, weight: .bold)
     case .SF17R:
       return .system(size: 17, weight: .regular)
+    case .SF16R:
+      return .system(size: 16, weight: .regular)
+    case .SF15R:
+      return .system(size: 15, weight: .regular)
     case .SF14R:
       return .system(size: 14, weight: .regular)
     case .SF12B:
@@ -29,36 +35,34 @@ extension Font {
       return .system(size: 10, weight: .bold)
     case .SF10R:
       return .system(size: 10, weight: .regular)
-    case .SF16R:
-      return .system(size: 16, weight: .regular)
-    case .SF15R:
-      return .system(size: 15, weight: .regular)
     }
   }
   
   public static var SFTitle: Font { style(.SFTitle) }
   public static var SF24B: Font { style(.SF24B) }
   public static var SF20B: Font { style(.SF20B) }
+  public static var SF17B: Font { style(.SF17B) }
   public static var SF17R: Font { style(.SF17R) }
+  public static var SF16R: Font { style(.SF16R) }
+  public static var SF15R: Font { style(.SF15R) }
   public static var SF14R: Font { style(.SF14R) }
   public static var SF12B: Font { style(.SF12B) }
   public static var SF12R: Font { style(.SF12R) }
   public static var SF10B: Font { style(.SF10B) }
   public static var SF10R: Font { style(.SF10R) }
-  public static var SF16R: Font { style(.SF16R) }
-  public static var SF15R: Font { style(.SF15R) }
 }
 
 public enum JulookFont {
   case SFTitle
   case SF24B
   case SF20B
+  case SF17B
   case SF17R
+  case SF16R
+  case SF15R
   case SF14R
   case SF12B
   case SF12R
   case SF10B
   case SF10R
-  case SF16R
-  case SF15R
 }
