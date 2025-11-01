e449a627170224edae766d83afe864aa160702e9
sunghun <leedool3003@gmail.com>
2025-11-01 16:15:40 +0900

    :hammer: [chore] SwiftNavigation 의존성 추가 및 TCA 업데이트

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -7,6 +7,7 @@ import ProjectDescription
 let packageSettings = PackageSettings(
   productTypes: [
     "ComposableArchitecture": .framework,
+    "SwiftNavigation": .framework,
     "TCACoordinators": .framework,
     "FirebaseAnalytics" : .staticLibrary,
     "FirebaseCrashlytics" : .staticLibrary,
@@ -19,6 +20,7 @@ let package = Package(
   name: "Julook",
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.11.0"),
+    .package(url: "https://github.com/pointfreeco/swift-navigation", from: "2.6.0"),
     .package(url: "https://github.com/johnpatrickmorgan/TCACoordinators", from: "0.10.1"),
     .package(url: "https://github.com/supabase/supabase-swift", from: "2.25.0"),
     .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.0.0"),
